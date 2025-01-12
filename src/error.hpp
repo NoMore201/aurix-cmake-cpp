d76d4f4515133bd51fd802aeb51df95205b1d774
Domenico Iezzi <domenico@iezzi.info>
2025-01-12 20:55:30 +0100

    Small refactoring to error handling

--- src/error.hpp
+++ src/error.hpp
@@ -4,7 +4,7 @@
 
 #define LIKELY(x) __builtin_expect(!!(x), 1)
 
-// TODO: implement properly
+// TODO: add error logging
 #define VERIFY(cond) (LIKELY(cond) ? static_cast<void>(0) : std::terminate())
 
-#define VERIFY_NOT_REACHED() std::terminate()
+#define VERIFY_NOT_REACHED() VERIFY(false)
