3d6de4de5a31fb79a34c7e9eb56bca650d2f029e
Domenico Iezzi <domenico.iezzi1@schindler.com>
2025-01-13 13:37:33 +0100

    Update gpio list with evaluation board LEDs

--- src/Cpu0_Main.cpp
+++ src/Cpu0_Main.cpp
@@ -2,22 +2,32 @@
 #include "gpio.hpp"
 #include "scu.hpp"
 
+#include <array>
+
 namespace
 {
 
-constexpr Hal::Gpio::Pin led{
-    .port     = 0,
-    .number   = 5,
-    .function = Hal::Gpio::Function::Output,
-    .speed    = Hal::Gpio::Speed::cmosAutomotiveSpeed1,
-    .mode     = Hal::Gpio::Mode::outputPushPullGeneral,
+constexpr std::array<Hal::Gpio::Pin, 2> led_list{
+    Hal::Gpio::Pin{
+        .port     = 33,
+        .number   = 4,
+        .function = Hal::Gpio::Function::Output,
+        .speed    = Hal::Gpio::Speed::cmosAutomotiveSpeed1,
+        .mode     = Hal::Gpio::Mode::outputPushPullGeneral,
+    },
+    Hal::Gpio::Pin{
+        .port     = 33,
+        .number   = 5,
+        .function = Hal::Gpio::Function::Output,
+        .speed    = Hal::Gpio::Speed::cmosAutomotiveSpeed1,
+        .mode     = Hal::Gpio::Mode::outputPushPullGeneral,
+    },
 };
 
 } // anonymous namespace
 
 extern "C"
 {
-
 // required by Ifx_Ssw_doCppInit
 void _init(void)
 {
@@ -28,7 +38,7 @@ void core0_main(void)
     Hal::Scu::disable_cpu_watchdog(Hal::Scu::get_cpu_password());
     Hal::Scu::disable_safety_watchdog(Hal::Scu::get_safety_password());
 
-    Hal::Gpio::configure(led);
+    Hal::Gpio::configure(led_list);
 
     while (true)
     {
