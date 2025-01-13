cd8ab0ed88b3201baf4b1d3f900df5589b4bb4e6
Domenico Iezzi <domenico.iezzi1@schindler.com>
2025-01-13 10:10:19 +0100

    Make verify macro call abort function

--- src/error.hpp
+++ src/error.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <exception>
+#include <cstdlib>
 
 #define LIKELY(x) __builtin_expect(!!(x), 1)
 
 // TODO: add error logging
-#define VERIFY(cond) (LIKELY(cond) ? static_cast<void>(0) : std::terminate())
+#define VERIFY(cond) (LIKELY(cond) ? static_cast<void>(0) : std::abort())
 
 #define VERIFY_NOT_REACHED() VERIFY(false)
